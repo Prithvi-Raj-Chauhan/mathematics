3d3e26e68b480bb66e375dd2a6858589efb33a9e
Prithvi Raj Chauhan <divyadollypadhy@outlook.com>
2022-08-05 20:59:45 +0530

    multiplication and division

--- fraction.py
+++ fraction.py
@@ -9,6 +9,18 @@ class Fraction:
     def __repr__(self) -> str:
         return f"{self.numerator}/{self.denominator}"
 
+    def reciprocal(self):
+        return Fraction(self.denominator, self.numerator)
+
+    def mul(self, other):
+        return Fraction(self.numerator * other.numerator, self.denominator * other.denominator)
+
+    def div(self, other):
+        return self.mul(other.reciprocal())
+
+
 if __name__ == '__main__':
     f1 = Fraction(1,2)
-    print(f1)
+    f2 = Fraction(3,4)
+    print(f1.mul(f2))
+    print(f1.div(f2))
